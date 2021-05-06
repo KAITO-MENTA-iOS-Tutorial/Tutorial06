6e8e5d9b1dd756224eb2e1f1489c17f36a0a5342
Kaito Hattori <kaichan394@gmail.com>
2021-05-06 15:12:11 +0900

    Initial Commit

---
+++ Tutorial06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tutorial06
+//
+//  Created by Kaito Hattori on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
